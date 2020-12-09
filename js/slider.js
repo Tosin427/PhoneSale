1831c91e2ba64b2b2a658670b125f74b451b807c
Tosin427 <48648548+Tosin427@users.noreply.github.com>
2020-12-09 19:46:17 +0100

    9th dec

--- js/slider.js
+++ js/slider.js
@@ -6,7 +6,7 @@ if(slider1) {
     new Glide(slider1, {
         type: 'carousel',
         startAt: 0,
-        // autoplay: 3000,
+        autoplay: 3000,
         hoverpause: true,
         perView: 1,
         animationDuration: 800,
