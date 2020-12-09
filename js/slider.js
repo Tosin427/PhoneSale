85daddcd78dcd8a4082eb2e2dd68ed3333557d56
Tosin427 <48648548+Tosin427@users.noreply.github.com>
2020-12-09 10:45:54 +0100

    far

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+// Our Carousel
+const slider1 = document.getElementById('glide__1');
+
+//Hero
+if(slider1) {
+    new Glide(slider1, {
+        type: 'carousel',
+        startAt: 0,
+        autoplay: 3000,
+        hoverpause: true,
+        perView: 1,
+        animationDuration: 800,
+        animationTimingFunc: 'linear',
+    }).mount();
+}
